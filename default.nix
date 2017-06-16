0bfac6333649fbf41f9b1aed28f2cf9f0e7d6127
Lahiru Pathirage http://lahiru.site <lpsandaruwan@gmail.com>
2017-06-16 20:51:18 +0530

    nix - deployment configuration

--- default.nix
+++ default.nix
@@ -1,14 +1,39 @@
-# Preare default Python environment
-
 with import <nixpkgs> {};
 
 
-(
-    pkgs.python35.buildEnv.override rec {
-        extraLibs = [
-            pkgs.python35Packages.flask
-            pkgs.python35Packages.pyyaml
-        ];
-    }
+let
+    buildInputs = [
+        nginx
+        python35Packages.python
+        python35Packages.flask
+        python35Packages.pyyaml
+    ];
 
-).env
+in {
+    network.description = "Local machine";
+
+    webserver = {
+        deployment = {
+            targetEnv = "virtualbox";
+            targetHost = "192.168.56.1";
+            virtualbox.memorySize = 1024;
+        };
+
+            services = {
+            nginx = {
+                enable = true;
+                config = '';
+                    http {
+                        include         ${nginx}/conf/mime.types;
+                        server_name     localhost;
+
+                        location / {
+                            proxy_pass http://localhost:5000;
+                        }
+                    }
+                '';
+            };
+        };
+    };
+}
+
